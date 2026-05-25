44315498251a72aac2d2b3b060a26c408572f362
rnagatarun <rnagatarun@gmail.com>
2026-05-26 00:16:47 +0530

    add LazyLoading class implementing the Lazy Loading singleton design pattern

---
+++ Creational Design Patterns/1. Singleton Pattern/LazyLoading.java
@@ -0,0 +1,50 @@
+public class LazyLoading {
+
+    /*
+     * LAZY LOADING SINGLETON
+     * ----------------------
+     * In Lazy Loading, the Singleton instance is created
+     * only when it is needed for the first time.
+     *
+     * The object is created only when getInstance()
+     * is called for the first time.
+     *
+     * Understanding:
+     * --------------
+     * - The instance initially starts as null.
+     * - On the first call to getInstance(), the object is created.
+     * - Future calls return the same already-created object.
+     *
+     * Pros:
+     * -----
+     * - Saves memory if the object is never used.
+     * - Object creation is delayed until actually required.
+     * - Better for heavy or expensive objects.
+     *
+     * Cons:
+     * -----
+     * - NOT thread-safe by default.
+     * - Multiple threads can create multiple objects simultaneously.
+     * - Requires synchronization in multi-threaded applications.
+     */
+
+    // Initially no object is created
+    private static LazyLoading instance;
+
+    // Private constructor prevents external object creation
+    private LazyLoading() {
+        System.out.println("Lazy Singleton Object Created");
+    }
+
+    // Object created only when needed
+    public static LazyLoading getInstance() {
+
+        // Create object only first time
+        if (instance == null) {
+            instance = new LazyLoading();
+        }
+
+        // Return same object afterwards
+        return instance;
+    }
+}
